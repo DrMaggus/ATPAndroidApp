c968a2df08e69d6360555a8b71084bab1b2fe1c8
Matthias Eiserloh <matthias_eiserloh@web.de>
2013-06-19 22:18:15 +0200

    Added textfield with fixed length

--- src/de/atp/requester/StartActivity.java
+++ src/de/atp/requester/StartActivity.java
@@ -1,8 +1,12 @@
 package de.atp.requester;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class StartActivity extends Activity {
 
@@ -10,8 +14,24 @@ public class StartActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
+		
+		Button button = (Button) findViewById(R.id.button1);
+		EditText editText = (EditText) findViewById(R.id.editTextCode);
+		if (editText.length()==5) button.isClickable();
+		
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				EditText editText = (EditText) findViewById(R.id.editTextCode);
+				TextView textview = (TextView) findViewById(R.id.textView1);
+				String message = editText.getText().toString();
+				textview.setText(message);
+			}
+		});	
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
