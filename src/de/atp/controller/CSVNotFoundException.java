43a1132f26136538a2faf1324fd1ae2bc34caaab
Meldanor <Bhaals@gmx.de>
2013-06-21 00:21:04 +0200

    Convert DataController to Singleton and add functions for answeredTime

---
+++ src/de/atp/controller/CSVNotFoundException.java
@@ -0,0 +1,15 @@
+package de.atp.controller;
+
+/**
+ * Indicates that the csv file for persistance is not found
+ * 
+ */
+public class CSVNotFoundException extends Exception {
+
+    private static final long serialVersionUID = -838903632898412494L;
+
+    public CSVNotFoundException() {
+        super();
+    }
+
+}
