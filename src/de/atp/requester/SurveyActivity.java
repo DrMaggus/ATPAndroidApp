c27217807176e78f058d071e1a47499f325450ed
Tabea Treutwein <tabea.treutwein@gmx.de>
2013-06-30 16:22:16 +0200

    last date at survey and new background in startactivity

--- src/de/atp/requester/SurveyActivity.java
+++ src/de/atp/requester/SurveyActivity.java
@@ -1,8 +1,10 @@
 package de.atp.requester;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
 
-import de.atp.controller.DataController;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -13,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import de.atp.controller.DataController;
 
 public class SurveyActivity extends Activity {
 
@@ -21,42 +24,39 @@ public class SurveyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
-        String contactQuestion = this.getResources().getString(R.string.contact_question, 3);
-        TextView question = (TextView) findViewById(R.id.contactQuestionView);
-        question.setText(contactQuestion);
+
+        contactQuestion();
         Button sendButton = (Button) findViewById(R.id.sendButton);
         sendButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                CharSequence meckerText = "";
                 boolean invalidInput = false;
                 long maxMinutes;
-                int minutes = 0, contacts = 0;
+                long minutes = 0, contacts = 0;
                 try {
                     maxMinutes = (new Date()).getTime() - DataController.instance().getLastAnsweredDate().getTime();
-                    maxMinutes /= 60000;
-                } catch(java.lang.NullPointerException e) {
+                    maxMinutes = TimeUnit.MILLISECONDS.toMinutes(maxMinutes);
+                } catch (java.lang.NullPointerException e) {
                     maxMinutes = Integer.MAX_VALUE;
                 }
                 try {
-                    minutes = getValue(R.id.minutes) + 60*getValue(R.id.hours);
+                    minutes = getValue(R.id.minutes) + TimeUnit.HOURS.toMinutes(getValue(R.id.hours));
                     contacts = getValue(R.id.numberOfContacts);
-                } catch(java.lang.NullPointerException e) {
+                } catch (java.lang.NullPointerException e) {
                     invalidInput = true;
-                    meckerText = v.getResources().getString(R.string.missingInput);
+                    toastText(v.getResources().getString(R.string.missingInput));
                 }
-                if(contacts == 0 ^ minutes == 0)
-                    meckerText = v.getResources().getString(R.string.contactsMinutesMismatch);
-                else if(!invalidInput && !checkValue(R.id.minutes, 59))
-                    meckerText = v.getResources().getString(R.string.invalidMinutes);
-                else if(minutes > maxMinutes)
-                    meckerText = v.getResources().getString(R.string.tooMuchMinutes);
-                else if(!invalidInput) {
+                if (contacts == 0 ^ minutes == 0)
+                    toastText(v.getResources().getString(R.string.contactsMinutesMismatch));
+                else if (!invalidInput && !checkValue(R.id.minutes, 59))
+                    toastText(v.getResources().getString(R.string.invalidMinutes));
+                else if (minutes > maxMinutes)
+                    toastText(v.getResources().getString(R.string.tooMuchMinutes));
+                else if (!invalidInput) {
                     saveValues();
                     return;
                 }
-                toastText(meckerText);
             }
-            
+
             private Integer getValue(int id) {
                 EditText editText = (EditText) findViewById(id);
                 String text = editText.getText().toString();
@@ -70,25 +70,45 @@ public class SurveyActivity extends Activity {
             private boolean checkValue(int id, int limit) {
                 try {
                     return getValue(id) <= limit;
-                } catch(java.lang.NullPointerException e) {
+                } catch (java.lang.NullPointerException e) {
                     return false;
                 }
             }
-            
+
             private void toastText(CharSequence text) {
                 Context context = getApplicationContext();
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
             }
-            
+
             private void saveValues() {
-                DataController.instance().completeQuestions(
-                        getValue(R.id.hours),
-                        getValue(R.id.minutes),
-                        getValue(R.id.numberOfContacts));
+                DataController.instance().completeQuestions(getValue(R.id.hours), getValue(R.id.minutes), getValue(R.id.numberOfContacts));
             }
         });
+    }
+
+    private void contactQuestion() {
+        Calendar cal = GregorianCalendar.getInstance();
+        Date date;
+        date = DataController.instance().getLastAnsweredDate();
+        if(date == null) {
+            errorToast();
+        }
+        cal.setTime(date);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int month = cal.get(Calendar.MONTH);
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        String contactQuestion = this.getResources().getString(R.string.contact_question, day, month, hour);
+        TextView question = (TextView) findViewById(R.id.contactQuestionView);
+        question.setText(contactQuestion);
+    }
+    
+    private void errorToast() {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, this.getResources().getString(R.id.databaseError), duration);
+        toast.show();
     }
 
     @Override
@@ -97,5 +117,4 @@ public class SurveyActivity extends Activity {
         getMenuInflater().inflate(R.menu.start, menu);
         return true;
     }
-
 }
