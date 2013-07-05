174acad63c2dbc3e6e9f7c6c72e06571d47cf4d6
Meldanor <Bhaals@gmx.de>
2013-07-05 10:10:11 +0200

    Move helper classes to private inner classes

--- src/de/atp/activity/SurveyActivity.java
+++ src/de/atp/activity/SurveyActivity.java
@@ -1,6 +1,7 @@
 package de.atp.activity;
 
 import org.joda.time.DateTime;
+import org.joda.time.Duration;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
@@ -12,7 +13,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import de.atp.controller.Alarm;
@@ -108,4 +112,72 @@ public class SurveyActivity extends Activity {
             finish();
         }
     };
+
+    private class SurveyOnClickListener implements OnClickListener {
+        private SurveyActivity activity;
+
+        public SurveyOnClickListener(SurveyActivity activity) {
+            this.activity = activity;
+        }
+
+        public void onClick(View v) {
+            boolean invalidInput = false;
+            long maxMinutes;
+            long minutes = 0, contacts = 0;
+            try {
+                Duration duration = new Duration(DataController.instance().getLastAnsweredDate(), DateTime.now());
+                maxMinutes = duration.getStandardMinutes();
+            } catch (java.lang.NullPointerException e) {
+                maxMinutes = Integer.MAX_VALUE;
+            }
+            try {
+                minutes = getValue(R.id.minutes) + 60 * getValue(R.id.hours);
+                contacts = getValue(R.id.numberOfContacts);
+            } catch (java.lang.NullPointerException e) {
+                invalidInput = true;
+                toastText(v.getResources().getString(R.string.surveyActivity_missingInput));
+            }
+            if (contacts == 0 ^ minutes == 0)
+                toastText(v.getResources().getString(R.string.surveyActivity_contactsMinutesMismatch));
+            else if (!invalidInput && !checkValue(R.id.minutes, 59))
+                toastText(v.getResources().getString(R.string.surveyActivity_invalidMinutes));
+            else if (minutes > maxMinutes)
+                toastText(v.getResources().getString(R.string.surveyActivity_tooMuchMinutes));
+            else if (!invalidInput) {
+                saveValues();
+                activity.startActivity(new Intent(activity, InfoActivity.class));
+                activity.finish();
+                return;
+            }
+        }
+
+        private Integer getValue(int id) {
+            EditText editText = (EditText) activity.findViewById(id);
+            String text = editText.getText().toString();
+            try {
+                return Integer.parseInt(text);
+            } catch (java.lang.NumberFormatException e) {
+                return null;
+            }
+        }
+
+        private boolean checkValue(int id, int limit) {
+            try {
+                return getValue(id) <= limit;
+            } catch (java.lang.NullPointerException e) {
+                return false;
+            }
+        }
+
+        private void toastText(CharSequence text) {
+            Context context = activity.getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+        private void saveValues() {
+            DataController.instance().completeQuestions(getValue(R.id.hours), getValue(R.id.minutes), getValue(R.id.numberOfContacts));
+        }
+    }
 }
--- src/de/atp/activity/TimetableActivity.java
+++ src/de/atp/activity/TimetableActivity.java
@@ -33,36 +33,36 @@ public class TimetableActivity extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
 
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_9am), 0, 9) );        
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_10am), 0, 10) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_11am), 0, 11) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_12pm), 0, 12) );
-        
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_1pm), 1, 13) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_2pm), 1, 14) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_3pm), 1, 15) );
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_9am), 0, 9));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_10am), 0, 10));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_11am), 0, 11));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_12pm), 0, 12));
 
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_4pm), 2, 16) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_5pm), 2, 17) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_6pm), 2, 18) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_7pm), 2, 19) );
-        
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_8pm), 3, 20) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_9pm), 3, 21) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_10pm), 3, 22) );
-        timeButtons.add( new RowButton( (ToggleButton)findViewById(R.id.button_11pm), 3, 23) );
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_1pm), 1, 13));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_2pm), 1, 14));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_3pm), 1, 15));
 
-        for (int i = 0; i < timeButtons.size(); i++) 
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_4pm), 2, 16));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_5pm), 2, 17));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_6pm), 2, 18));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_7pm), 2, 19));
+
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_8pm), 3, 20));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_9pm), 3, 21));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_10pm), 3, 22));
+        timeButtons.add(new RowButton((ToggleButton) findViewById(R.id.button_11pm), 3, 23));
+
+        for (int i = 0; i < timeButtons.size(); i++)
             timeButtons.get(i).getTogglebutton().setOnClickListener(this);
 
         // Create done button
         button_done = (Button) findViewById(R.id.button_done);
         button_done.setOnClickListener(this);
-        //Create Cancel button
+        // Create Cancel button
         button_cancel = (Button) findViewById(R.id.timetable_cancel);
         button_cancel.setOnClickListener(this);
     }
-    
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -73,32 +73,64 @@ public class TimetableActivity extends Activity implements OnClickListener {
             getButtonFromTime(alarms.get(i).getHourOfDay()).getTogglebutton().setChecked(true);
         }
     }
-    
+
     @Override
     public void onClick(View v) {
 
         switch (v.getId()) {
-            //row 1
-            case R.id.button_9am:   toggle(0, 9);  break;
-            case R.id.button_10am:  toggle(0, 10); break;
-            case R.id.button_11am:  toggle(0, 11); break;
-            case R.id.button_12pm:  toggle(0, 12); break;
-            //row 2
-            case R.id.button_1pm:   toggle(1, 13);  break;
-            case R.id.button_2pm:   toggle(1, 14);  break;
-            case R.id.button_3pm:   toggle(1, 15);  break;
-            //row 3
-            case R.id.button_4pm:   toggle(2, 16);  break;
-            case R.id.button_5pm:   toggle(2, 17);  break;
-            case R.id.button_6pm:   toggle(2, 18);  break;
-            case R.id.button_7pm:   toggle(2, 19);  break;
-            //row 4
-            case R.id.button_8pm:   toggle(3, 20);  break;
-            case R.id.button_9pm:   toggle(3, 21);  break;
-            case R.id.button_10pm:  toggle(3, 22); break;
-            case R.id.button_11pm:  toggle(3, 23); break;         
+        // row 1
+            case R.id.button_9am :
+                toggle(0, 9);
+                break;
+            case R.id.button_10am :
+                toggle(0, 10);
+                break;
+            case R.id.button_11am :
+                toggle(0, 11);
+                break;
+            case R.id.button_12pm :
+                toggle(0, 12);
+                break;
+            // row 2
+            case R.id.button_1pm :
+                toggle(1, 13);
+                break;
+            case R.id.button_2pm :
+                toggle(1, 14);
+                break;
+            case R.id.button_3pm :
+                toggle(1, 15);
+                break;
+            // row 3
+            case R.id.button_4pm :
+                toggle(2, 16);
+                break;
+            case R.id.button_5pm :
+                toggle(2, 17);
+                break;
+            case R.id.button_6pm :
+                toggle(2, 18);
+                break;
+            case R.id.button_7pm :
+                toggle(2, 19);
+                break;
+            // row 4
+            case R.id.button_8pm :
+                toggle(3, 20);
+                break;
+            case R.id.button_9pm :
+                toggle(3, 21);
+                break;
+            case R.id.button_10pm :
+                toggle(3, 22);
+                break;
+            case R.id.button_11pm :
+                toggle(3, 23);
+                break;
 
-            case R.id.timetable_cancel: finish(); break;
+            case R.id.timetable_cancel :
+                finish();
+                break;
 
             case R.id.button_done :
                 if (!buttonCheck()) {
@@ -107,24 +139,24 @@ public class TimetableActivity extends Activity implements OnClickListener {
                     // Persist next alarm times
                     DataController controller = DataController.instance();
                     List<LocalTime> oldAlarms = controller.getTodaysAlarms();
-                  //TODO: check => sometimes times don't get saved (randomly?!) <=> csv gets fucked up
-                    for (int i = 0,numEntries = 0; i < timeButtons.size(); i++) {
-                        if(timeButtons.get(i).getTogglebutton().isChecked()) {
+                    // TODO: check => sometimes times don't get saved
+                    // (randomly?!) <=> csv gets fucked up
+                    for (int i = 0, numEntries = 0; i < timeButtons.size(); i++) {
+                        if (timeButtons.get(i).getTogglebutton().isChecked()) {
                             if (oldAlarms.isEmpty())
-                                controller.createDummyRow( timeButtons.get(i).getTime() , 0);
-                            else 
-                                if (timeChoiceIsValid( oldAlarms.get(numEntries).getHourOfDay() , timeButtons.get(i).getTime() ))
-                                    controller.changeAlarmTime( oldAlarms.get(numEntries).getHourOfDay(), 0, timeButtons.get(i).getTime(), 0);
-                                else
-                                    Toast.makeText(this, "Zeiten wurden so gewaehlt, dass sie heute nicht mehr erreichbar bzw. wieder erreichbar sind", 6).show();
-                                numEntries++;
-                            }
+                                controller.createDummyRow(timeButtons.get(i).getTime(), 0);
+                            else if (timeChoiceIsValid(oldAlarms.get(numEntries).getHourOfDay(), timeButtons.get(i).getTime()))
+                                controller.changeAlarmTime(oldAlarms.get(numEntries).getHourOfDay(), 0, timeButtons.get(i).getTime(), 0);
+                            else
+                                Toast.makeText(this, "Zeiten wurden so gewaehlt, dass sie heute nicht mehr erreichbar bzw. wieder erreichbar sind", 6).show();
+                            numEntries++;
+                        }
                     }
                     setFirstAlarm();
                     startActivity(new Intent(this, InfoActivity.class));
                     finish();
                 }
-                
+
         }
     }
 
@@ -134,9 +166,9 @@ public class TimetableActivity extends Activity implements OnClickListener {
                 return timeButtons.get(i);
         return null;
     }
-    
+
     private void setButtonRowToRed(int row) {
-        for (int i = 0; i < timeButtons.size(); i++) 
+        for (int i = 0; i < timeButtons.size(); i++)
             if (timeButtons.get(i).getRow() == row)
                 timeButtons.get(i).getTogglebutton().setChecked(false);
     }
@@ -145,15 +177,13 @@ public class TimetableActivity extends Activity implements OnClickListener {
         setButtonRowToRed(row);
         getButtonFromTime(hour).getTogglebutton().setChecked(true);
     }
-    
+
     private boolean timeChoiceIsValid(int oldTime, int newTime) {
         DataController dc = DataController.instance();
         if (!dc.getTodaysAlarms().isEmpty()) {
-            if (dc.getTodaysAlarms().get( getButtonFromTime( oldTime ).getRow() ).isAfter( LocalTime.now() ) &&
-                    new LocalTime( newTime, 0, 0).isBefore( LocalTime.now() ) )
+            if (dc.getTodaysAlarms().get(getButtonFromTime(oldTime).getRow()).isAfter(LocalTime.now()) && new LocalTime(newTime, 0, 0).isBefore(LocalTime.now()))
                 return false;
-            if (dc.getTodaysAlarms().get( getButtonFromTime( oldTime ).getRow() ).isBefore( LocalTime.now() ) &&
-                    new LocalTime( newTime, 0, 0).isAfter( LocalTime.now() ) )
+            if (dc.getTodaysAlarms().get(getButtonFromTime(oldTime).getRow()).isBefore(LocalTime.now()) && new LocalTime(newTime, 0, 0).isAfter(LocalTime.now()))
                 return false;
         }
         return true;
@@ -164,7 +194,7 @@ public class TimetableActivity extends Activity implements OnClickListener {
         boolean row2 = false;
         boolean row3 = false;
         boolean row4 = false;
-        
+
         for (int i = 0; i < timeButtons.size(); i++) {
             if (timeButtons.get(i).getRow() == 0)
                 row1 = row1 || timeButtons.get(i).getTogglebutton().isChecked();
@@ -175,7 +205,7 @@ public class TimetableActivity extends Activity implements OnClickListener {
             else if (timeButtons.get(i).getRow() == 3)
                 row4 = row4 || timeButtons.get(i).getTogglebutton().isChecked();
         }
-        
+
         return row1 && row2 && row3 && row4;
     }
 
@@ -202,6 +232,31 @@ public class TimetableActivity extends Activity implements OnClickListener {
         }
 
         alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTime.getMillis(), pendingIntent);
+    }
+
+    private class RowButton {
+        private ToggleButton togglebutton;
+        private int row;
+        private int time;
+
+        public RowButton(ToggleButton togglebutton, int row, int time) {
+            this.togglebutton = togglebutton;
+            this.row = row;
+            this.time = time;
+        }
+
+        public ToggleButton getTogglebutton() {
+            return togglebutton;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
     }
 
 }
