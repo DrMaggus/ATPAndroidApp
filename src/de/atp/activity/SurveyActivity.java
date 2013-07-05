b168bbead7ca212738865532b273cd054fcb8635
Meldanor <Bhaals@gmx.de>
2013-07-05 10:08:10 +0200

    Mark member variables as private

--- src/de/atp/activity/SurveyActivity.java
+++ src/de/atp/activity/SurveyActivity.java
@@ -20,29 +20,32 @@ import de.atp.controller.DataController;
 import de.atp.requester.R;
 
 public class SurveyActivity extends Activity {
-    AlarmManager alarmManager;
-    PendingIntent pendingIntent;
-    Intent myIntent;
+
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private Intent myIntent;
+
     @Override
-    // this is the worst function I've written in years...
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
+
+        Button sendButton = (Button) findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new SurveyOnClickListener(this));
+
         registerReceiver(FinishHim, new IntentFilter("finishActivity"));
         setAlarmManager();
         contactQuestion();
-        Button sendButton = (Button) findViewById(R.id.sendButton);
-        sendButton.setOnClickListener(new SurveyOnClickListener(this));
     }
 
     private void contactQuestion() {
         DataController dataController = DataController.instance();
-        if(dataController==null) {
+        if (dataController == null) {
             errorToast();
             return;
         }
         DateTime date = DataController.instance().getLastAnsweredDate();
-        if(date==null) {
+        if (date == null) {
             errorToast();
             return;
         }
@@ -65,24 +68,24 @@ public class SurveyActivity extends Activity {
     private void setup() {
         if (myIntent == null)
             myIntent = new Intent(this, Alarm.class);
-        if(alarmManager == null)
-            alarmManager=(AlarmManager)(this.getSystemService( Context.ALARM_SERVICE ));
-        if(pendingIntent == null)
+        if (alarmManager == null)
+            alarmManager = (AlarmManager) (this.getSystemService(Context.ALARM_SERVICE));
+        if (pendingIntent == null)
             pendingIntent = PendingIntent.getService(this, 0, myIntent, 0);
     }
 
     /**
      * call this method if you want to create a new Alarm
      */
-    private void setAlarmManager(){
+    private void setAlarmManager() {
         setup();
         DataController controller = DataController.instance();
-        
+
         if (controller == null) {
             Toast.makeText(getBaseContext(), "Can't load controller!", Toast.LENGTH_LONG).show();
             return;
         }
-       
+
         DateTime alarmTime = controller.getNextAlarm();
 
         // Error while founding the alarm time
@@ -90,11 +93,11 @@ public class SurveyActivity extends Activity {
             Toast.makeText(getBaseContext(), "No alarm found!", Toast.LENGTH_LONG).show();
             return;
         }
-        
+
         alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTime.getMillis(), pendingIntent);
-        
+
     }
-    
+
     /**
      * BroadcastReceiver to finish the old SurveyActivities, if already a new
      * one starts
